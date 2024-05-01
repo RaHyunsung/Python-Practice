ba1bd3a1636f51b19232ea40ae8e9430d75873b1
rahyunsung <rah@wsc.school.nz>
2024-05-02 11:53:47 +1200

    Greet with username

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 username = input("What's your name? ")
 print()
 # Greet the user and introduce the quiz
-
+print("Hello! {}. This is a quiz about Python.".format(username))
+print()
 # Ask the user a question
 
 # Tell them the correct answer
