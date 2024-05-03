03bc3be6ceb15fa77da8d8220c03571acb910d53
rahyunsung <rah@wsc.school.nz>
2024-05-03 12:54:29 +1200

    Score System

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Ask the user their name and save it
 username = input("What's your name? ")
+score = 0
 print()
 # Greet the user and introduce the quiz
 print("Hello! {}. This is a quiz about Python.".format(username))
@@ -10,6 +11,7 @@ print()
 # Tell them the correct answer
 if response == "#":
     print("The answer was #. You are correct!")
+    score += 1
 elif response == "":
     print("The answer was #. You skipped the question!")
 else:
@@ -20,6 +22,7 @@ print()
 # Tell them the correct answer
 if response_question2 == 'print("Hello world!")':
     print('The answer was print("Hello world!"). You are correct!')
+    score += 1
 elif response_question2 == "":
     print('The answer was print("Hello world!"). You skipped the question!')
 else:
@@ -30,6 +33,7 @@ print()
 # Tell them the correct answer
 if response_question3 == "input()":
     print("The answer was input(). You are correct!")
+    score += 1
 elif response_question3 == "":
     print("The answer was input(). You skipped the question!")
 else:
@@ -40,6 +44,7 @@ print()
 # Tell them the correct answer
 if response_question4 == '"':
     print('The answer was ". You are correct!')
+    score += 1
 elif response_question4 == "":
     print('The answer was ". You skipped the question!')
 else:
@@ -50,6 +55,7 @@ print()
 # Tell them the correct answer
 if response_question5 == "=":
     print("The answer was =. You are correct!")
+    score += 1
 elif response_question5 == "":
     print("The answer was =. You skipped the question!")
 else:
@@ -60,6 +66,7 @@ print()
 # Tell them the correct answer
 if response_question6 == ".py":
     print("The answer was .py. You are correct!")
+    score += 1
 elif response_question6 == "":
     print("The answer was .py. You skipped the question!")
 else:
@@ -70,8 +77,10 @@ print()
 # Tell them the correct answer
 if response_question7 == "for":
     print("The answer was for. You are correct!")
+    score += 1
 elif response_question7 == "":
     print("The answer was for. You skipped the question!")
 else:
     print("The answer was for. You are incorrect!")
 # End the quiz
+print("Great job! Your score is {} out of 7!".format(score))
